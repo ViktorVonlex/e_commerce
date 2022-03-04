5f0a455cfe5e24a164648fa6df63f6d97d8625eb
Jiri Suster <susj05@vse.cz>
2022-03-04 19:49:14 +0100

    first push

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">Vayne Skins Shop</a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route exact path="/product/:id" element={<ProductScreen/>}></Route>
+            <Route exact path="/" element={<HomeScreen/>}></Route>
+          </Routes>
+        </main>
+        <footer className="row center">
+          All right reserved
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
